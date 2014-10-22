a0a1e8359bc5f304f8eb7a71e95473733cf3898a
Dan W <git@iamdanw.com>
2014-10-22 18:39:03 +0100

    Didn't fix duplicate relations in the data, but have discreetly swept them under the rug

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ActiveRecord::Base
-  has_and_belongs_to_many :movies, join_table: 'movies_genres'
+  has_and_belongs_to_many :movies, -> { uniq }, join_table: 'movies_genres'
 
   def words
     name.scan(/(?:
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,3 @@
 class Movie < ActiveRecord::Base
-  has_and_belongs_to_many :genres, join_table: 'movies_genres'
+  has_and_belongs_to_many :genres, -> { uniq }, join_table: 'movies_genres'
 end
