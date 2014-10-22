82db56444f714c50ce5f35fd027028439a1148ac
Frankie Roberto <frankie@frankieroberto.com>
2014-10-22 13:01:52 +0100

    Bugfix

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,7 +2,7 @@ class WordsController < ApplicationController
 
   def show
     @word = params[:id]
-    @genres = Genre.where(["name like ?", "#{@word}"]).limit(10)
+    @genres = Genre.where(["name like ?", "%#{@word}%"]).order("random()").limit(10)
   end
 
 end
