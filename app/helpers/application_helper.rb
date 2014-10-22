33d6ceac430ac4bb1eeed2818e8206fc362da39e
Frankie Roberto <frankie@frankieroberto.com>
2014-10-22 16:26:25 +0100

    More Regex

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def words_links(words)
+
+    words.collect do |word|
+
+      if word == "Movies"
+        "Movies"
+      else
+        link_to(word, word_path(word))
+      end
+
+    end.join(" ").html_safe
+
+  end
+
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -6,6 +6,16 @@ class Genre < ActiveRecord::Base
       (?:from\sthe\s[\d]+s) | 
       (?:directed\sby\s[^\s]+\s[^\s]+) |
       (?:for\sages\s[\d]+\sto\s[\d]+) |
+      (?:starring\s[^\s]+(?:\s[^\s]+)*) |
+      (?:[^\s]+\s\&\s[^\s]+) |
+      (?:on\sBlu\-ray) |
+      (?:based\son\s
+        (?:a\s)?
+        (?:[Rr]eal\s)?
+        [^\s]+
+        (?:\s(?:[Bb]books|[Ll]iterature))?
+        (?:\sby\s[^\s]+(?:\s[A-Z][^\s]+)*)?
+      ) |
       (?:[^\s]+)
     )/x)
   end
