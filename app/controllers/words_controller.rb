bad41f324302344a3892e064cc3bf721c768cd7d
Frankie Roberto <frankie@frankieroberto.com>
2014-10-23 20:04:55 +0100

    Prioritise genres with movies

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,7 +2,17 @@ class WordsController < ApplicationController
 
   def show
     @word = params[:id]
-    @genres = Genre.where(["lower(name) like ?", "%#{@word.downcase}%"]).order("random()").limit(30)
+    
+    limit = 30
+
+    @genres = Genre.where(["lower(name) like ?", "%#{@word.downcase}%"]).where("movies_count > 0").order("random()").limit(limit)
+
+    if @genres.length < limit
+      @genres.push(Genre.where(["lower(name) like ?", "%#{@word.downcase}%"]).where("movies_count = 0").order("random()").limit(limit - @genres.length))
+    end
+
+    @genres = @genres.shuffle
+
   end
 
 end
