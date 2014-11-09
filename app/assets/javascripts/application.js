e8237e989d74020b4c8cc9725834e2fee150f310
Luke Bacon <luke@equivalentideas.com>
2014-11-09 20:11:28 +1100

    add function for making ajax calls
    from @adactio book DOM Scripting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,6 +29,22 @@ function insertAfter(newElement,targetElement) {
   }
 }
 
+// for making ajax calls
+function getHTTPObject() {
+  if (typeof XMLHttpRequest == "undefined") {
+    XMLHttpRequest = function () {
+      try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+        catch (e) {}
+      try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+        catch (e) {}
+      try { return new ActiveXObject("Msxml2.XMLHTTP"); }
+        catch (e) {}
+      return false;
+    };
+  }
+  return new XMLHttpRequest();
+}
+
 
 }
 
